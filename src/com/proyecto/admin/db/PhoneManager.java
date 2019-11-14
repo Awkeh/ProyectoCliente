029652ec54e02b6b71ebebe4a91fed06a0fd432f
Neo.h <nicolas.mg.uy@gmail.com>
2019-11-14 20:47:16 -0300

    Implemented managers for Phones and Zones

--- src/com/proyecto/admin/db/PhoneManager.java
+++ src/com/proyecto/admin/db/PhoneManager.java
@@ -1,7 +1,88 @@
 package com.proyecto.admin.db;
 
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.proyecto.admin.utils.Alert;
+import com.proyecto.entidades.Telefono;
+import com.proyecto.porotos.TelefonoBeanRemote;
+
 public class PhoneManager {
 
-	// TODO: Phone manager
+	private static TelefonoBeanRemote bean = null;
+
+	private static boolean isBeanGTG() {
+		return bean != null;
+	}
+
+	private static void findBean() {
+		try {
+			bean = (TelefonoBeanRemote) InitialContext.doLookup("ProyectoPorotos/TelefonoBean!com.proyecto.porotos.TelefonoBeanRemote");
+		}
+		catch (NamingException ne) {
+			Alert.error("Error", ne.getMessage());
+			ne.printStackTrace();
+		}
+	}
+
+	private static void findBeanifNull() {
+		if(!isBeanGTG()) {
+			findBean();
+		}
+	}
+
+	public static boolean create(int numb) {
+		findBeanifNull();
+
+		try {
+			return bean.create(numb);
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static Telefono update(int numb, int newNumb) {
+		findBeanifNull();
+
+		try {
+			return bean.update(numb, newNumb);
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Telefono find(int numb) {
+		findBeanifNull();
+
+		try {
+			return bean.find(numb);
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static List<Telefono> getAll() {
+		findBeanifNull();
+
+		try {
+			return bean.getAll();
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
--- src/com/proyecto/admin/db/ZoneManager.java
+++ src/com/proyecto/admin/db/ZoneManager.java
@@ -1,7 +1,88 @@
 package com.proyecto.admin.db;
 
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.proyecto.admin.utils.Alert;
+import com.proyecto.entidades.Zona;
+import com.proyecto.porotos.ZonaBeanRemote;
+
 public class ZoneManager {
 
-	// TODO: Zone manager
+	private static ZonaBeanRemote bean = null;
+
+	private static boolean isBeanGTG() {
+		return bean != null;
+	}
+
+	private static void findBean() {
+		try {
+			bean = (ZonaBeanRemote) InitialContext.doLookup("ProyectoPorotos/ZonaBean!com.proyecto.porotos.ZonaBeanRemote");
+		}
+		catch (NamingException ne) {
+			Alert.error("Error", ne.getMessage());
+			ne.printStackTrace();
+		}
+	}
+
+	private static void findBeanifNull() {
+		if(!isBeanGTG()) {
+			findBean();
+		}
+	}
+
+	public static boolean create(String name) {
+		findBeanifNull();
+
+		try {
+			return bean.create(name);
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static Zona update(String name, String newName) {
+		findBeanifNull();
+
+		try {
+			return bean.update(name, newName);
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Zona find(String name) {
+		findBeanifNull();
+
+		try {
+			return bean.find(name);
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static List<Zona> getAll() {
+		findBeanifNull();
+
+		try {
+			return bean.getAll();
+		}
+		catch (Exception e) {
+			Alert.error("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
